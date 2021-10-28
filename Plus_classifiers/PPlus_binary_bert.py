5c9126045468a8281146b4959318bb187bfd9d63
casszhao <cass790412@gmail.com>
2021-10-28 11:18:33 +0100

    add plus_classifiers

--- Plus_classifiers/PPlus_binary_bert.py
+++ Plus_classifiers/PPlus_binary_bert.py
@@ -246,8 +246,7 @@ all_targets_list = np.transpose(all_targets_array.reshape(len(list_of_label), le
 results_df = pd.DataFrame(list(zip(text_list,all_targets_list,binary_pred_list,binary_prob_list)),
                                columns =['Text', 'Ground truth', 'Prediction', 'Probability'])
 
-parser.add_argument("--bert_model", "-b", default='bert-base-uncased')
-# other option allenai/scibert_scivocab_uncased
+
 
 if args.bert_model == 'allenai/scibert_scivocab_uncased':
     results_df_name = 'scibert_' + str(args.max_len) + 'len_' + str(args.train_batch_size) + 'b_' + str(args.epoch) + 'e_'+ 'binary_results.csv'
