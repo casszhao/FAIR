8b5befb0f72b03a92c6b900b85000d9c463ef4e9
casszhao <cass790412@gmail.com>
2021-11-25 20:55:16 +0000

    fix binary base

--- results/investigation.py
+++ results/investigation.py
@@ -6,6 +6,12 @@ from ast import literal_eval
 import csv
 import pandas as pd
 import seaborn as sns
+from sklearn.utils import resample
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from matplotlib import pyplot
+
+
 sns.set(style="darkgrid", font_scale=1.2)
 
 fileName = 'scibert_500len_8b_20e_binary_results.csv'
@@ -22,13 +28,52 @@ prob_array = np.array([np.array(xi) for xi in probability])
 
 list_of_label = ['Place', 'Race', 'Occupation', 'Gender', 'Religion', 'Education', 'Socioeconomic', 'Social', 'Plus']
 
-binary_f1_score_micro = f1_score(targets_array, pred_array, average='micro')
-binary_f1_score_macro = f1_score(targets_array, pred_array, average='macro')
-print('micro: ', binary_f1_score_micro, 'macro: ', binary_f1_score_macro)
+
+# load dataset
+# values = data.values
+
+# configure bootstrap
+n_iterations = 1000
+n_size = int(len(df) * 0.8)
+# run bootstrap
+stats = list()
+for i in range(n_iterations):
+
+    temp_df = df.sample(n_size, replace=True)
+
+    pred_label = df['Prediction']
+    target = df['Ground truth']
+    probability = df['Probability']
+
+    pred_array = np.array([np.array(xi) for xi in pred_label])
+    targets_array = np.array([np.array(xi) for xi in target])
+    prob_array = np.array([np.array(xi) for xi in probability])
+
+    # roc = roc_auc_score(targets_array, prob_array)
+    # f1_score_micro = f1_score(targets_array, pred_array, average='micro')
+    # f1_score_macro = f1_score(targets_array, pred_array, average='macro')
+    metrics = precision_score(targets_array, pred_array, average='macro')
+    stats.append(metrics)
+
+# plot scores
+pyplot.hist(stats, bins=100)
+pyplot.show()
+# confidence intervals
+alpha = 0.95
+p = ((1.0-alpha)/2.0) * 100
+lower = max(0.0, np.percentile(stats, p))
+p = (alpha+((1.0-alpha)/2.0)) * 100
+upper = min(1.0, np.percentile(stats, p))
+print('%.1f confidence interval %.1f%% and %.1f%%' % (alpha*100, lower*100, upper*100))
 
 
+stop
+
+f1_score_micro = f1_score(targets_array, pred_array, average='micro')
+f1_score_macro = f1_score(targets_array, pred_array, average='macro')
 roc = roc_auc_score(targets_array, prob_array)
 print('roc: ', roc)
+print('micro: ', f1_score_micro, 'macro: ', f1_score_macro)
 
 
 def one_label_f1(label_index):
@@ -59,59 +104,25 @@ for i, label in enumerate(list_of_label):
 avg_brier = sum(all_brier)/len(all_brier)
 print('avg brier :', avg_brier)
 
+
+
+'''
+for plot, do not delete
+'''
 # print(plt_list)
-print(len(plt_list))
-print(len(plt_list[1]))
-temp_df = pd.DataFrame(plt_list).transpose()
-temp_df.columns = list_of_label
+# print(len(plt_list))
+# print(len(plt_list[1]))
+# temp_df = pd.DataFrame(plt_list).transpose()
+# temp_df.columns = list_of_label
 
-for i, label in enumerate(list_of_label):
-
-    sns.displot(
-      data=temp_df,
-      x=temp_df[label],
-      kind="hist",
-      aspect=1.4,
-      log_scale=10
-    )
-    plt.show()
-# ax = temp_df.plot.hist(bins=12, alpha=0.5)
-# sns.displot(data=temp_df, col='Days', col_wrap=4, x='Visitors')
-
-
-# temp_df.plot(kind='hist',
-#         alpha=0.7,
-#         bins=30,
-#         title='Histogram Of Test Scores',
-#         rot=45,
-#         grid=True,
-#         figsize=(12,8),
-#         fontsize=15,
-#         )
-# plt.xlabel('Test Score')
-# plt.ylabel("Number Of Students")
-
-# print(plt_list)
-# fig, axs = plt.subplots(3, 3)
 # for i, label in enumerate(list_of_label):
-#     if i < 3: # 0, 1, 2,
-#         axs_x = 0
-#         axa_y = 0+i
-#     elif i < 6: # 3,4,5
-#         axs_x = 1
-#         axa_y = i-3
-#     else: # 6,7,8
-#         axs_x = 2
-#         axa_y = i-6
 #
-#     print(axs_x, axa_y)
-#     print(len(plt_list[i]))
-#     axs[axs_x, axa_y].plot(plt_list[i])
-#     axs[axs_x, axa_y].set_title(list_of_label[i])
-# fig.tight_layout()
-# plt.show()
+#     sns.displot(
+#       data=temp_df,
+#       x=temp_df[label],
+#       kind="hist",
+#       aspect=1.4,
+#       log_scale=10
+#     )
+#     plt.show()
 
-
-binary_f1_score_micro = f1_score(targets_array, pred_array, average='micro')
-binary_f1_score_macro = f1_score(targets_array, pred_array, average='macro')
-print(binary_f1_score_micro, binary_f1_score_macro)
